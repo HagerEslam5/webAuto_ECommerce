285c88ff560d139c0f052497e508eec010a30a82
HagerEslam5 <159949293+HagerEslam5@users.noreply.github.com>
2024-07-25 13:54:49 +0300

    Update T08_SortItems.java

--- src/test/java/tests/T08_SortItems.java
+++ src/test/java/tests/T08_SortItems.java
@@ -16,11 +16,18 @@ public class T08_SortItems extends BaseTest {
 	@Description("Verify that a user can sort products by price")
 	@Severity(SeverityLevel.MINOR)
 
-	public void sortByPriceTest() {
+	public void sortByPriceTest() throws InterruptedException {
 		new HomePage(driver).HoverOnApparel();
 		P08_Clothing clothing = new P08_Clothing(driver);
 		List<Integer> expectedPrices = clothing.goToClothing().getPricesAndSortThem();
 		List<Integer> actualPrices = clothing.sortByPrice().getTheDisplayedPrices();
+		
+		for(int pricer:expectedPrices) {
+			System.out.println(pricer);
+		}
+		for(int pricer:actualPrices) {
+			System.out.println(pricer);
+		}
 		Assert.assertTrue(expectedPrices.equals(actualPrices));
 	}
 
